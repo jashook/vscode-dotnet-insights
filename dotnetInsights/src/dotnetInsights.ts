a82c3352574618f9d71cba9d809ceccec2d55dc6
jashook <jarret_shook@outlook.com>
2021-03-08 07:37:13 -0800

    initial commit

---
+++ dotnetInsights/src/dotnetInsights.ts
@@ -0,0 +1,59 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
+    private _onDidChangeTreeData: vscode.EventEmitter<Dependency | undefined | void> = new vscode.EventEmitter<Dependency | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<Dependency | undefined | void> = this._onDidChangeTreeData.event;
+
+    constructor(private workspaceRoot: string) {
+	}
+
+    getTreeItem(element: Dependency): vscode.TreeItem {
+		return element;
+	}
+
+    getChildren(element?: Dependency): Thenable<Dependency[]> {
+		if (!this.workspaceRoot) {
+			vscode.window.showInformationMessage('No dependency in empty workspace');
+			return Promise.resolve([]);
+		}
+
+        return Promise.resolve([]);
+    }
+
+}
+
+export class Dependency extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+		private readonly version: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(label, collapsibleState);
+
+		this.tooltip = `${this.label}-${this.version}`;
+		this.description = this.version;
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+
+	contextValue = 'dependency';
+}
+
+export class DotnetInsights {
+    public ilDasmPath: string;
+    public ilDasmVersion: string;
+    public sdkVersions: string[];
+
+    constructor() {
+        this.ilDasmPath = "";
+        this.ilDasmVersion = "";
+        this.sdkVersions = [] as string[];
+    }
+}
