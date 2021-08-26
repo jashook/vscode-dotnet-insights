805a7f7974531e966eb6cf29be05fc2541e22c38
jashoo <jashoo@microsoft.com>
2021-08-26 06:17:26 -0700

    Setup windows build

---
+++ profiler/native/asm/amd64/windows/asm_helper.asm
@@ -0,0 +1,132 @@
+EXTERN EnterStub:PROC
+EXTERN ExitStub:PROC
+EXTERN TailcallStub:PROC
+
+_text SEGMENT PARA 'CODE'
+
+ALIGN 16
+PUBLIC EnterWrapper
+
+EnterWrapper PROC FRAME
+
+    PUSH RAX
+    .PUSHREG RAX
+    PUSH RCX
+    .PUSHREG RCX
+    PUSH RDX
+    .PUSHREG RDX
+    PUSH R8
+    .PUSHREG R8
+    PUSH R9
+    .PUSHREG R9
+    PUSH R10
+    .PUSHREG R10
+    PUSH R11
+    .PUSHREG R11
+
+    SUB RSP, 20H
+    .ALLOCSTACK 20H
+
+    .ENDPROLOG
+
+    CALL EnterStub
+
+    ADD RSP, 20H
+
+    POP R11
+    POP R10
+    POP R9
+    POP R8
+    POP RDX
+    POP RCX
+    POP RAX
+
+    RET
+
+EnterWrapper ENDP
+
+ALIGN 16
+PUBLIC ExitWrapper
+
+ExitWrapper PROC FRAME
+
+    PUSH RAX
+    .PUSHREG RAX
+    PUSH RCX
+    .PUSHREG RCX
+    PUSH RDX
+    .PUSHREG RDX
+    PUSH R8
+    .PUSHREG R8
+    PUSH R9
+    .PUSHREG R9
+    PUSH R10
+    .PUSHREG R10
+    PUSH R11
+    .PUSHREG R11
+
+    SUB RSP, 20H
+    .ALLOCSTACK 20H
+
+    .ENDPROLOG
+
+    CALL ExitStub
+
+    ADD RSP, 20H
+
+    POP R11
+    POP R10
+    POP R9
+    POP R8
+    POP RDX
+    POP RCX
+    POP RAX
+
+    RET
+
+ExitWrapper ENDP
+
+ALIGN 16
+PUBLIC TailcallWrapper
+
+TailcallWrapper PROC FRAME
+
+    PUSH RAX
+    .PUSHREG RAX
+    PUSH RCX
+    .PUSHREG RCX
+    PUSH RDX
+    .PUSHREG RDX
+    PUSH R8
+    .PUSHREG R8
+    PUSH R9
+    .PUSHREG R9
+    PUSH R10
+    .PUSHREG R10
+    PUSH R11
+    .PUSHREG R11
+
+    SUB RSP, 20H
+    .ALLOCSTACK 20H
+
+    .ENDPROLOG
+
+    CALL TailcallStub
+
+    ADD RSP, 20H
+
+    POP R11
+    POP R10
+    POP R9
+    POP R8
+    POP RDX
+    POP RCX
+    POP RAX
+
+    RET
+
+TailcallWrapper ENDP
+
+_text ENDS
+
+END
